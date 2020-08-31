c1a152d7a4beff49ce5a4eaeb4c034fb0e4784b0
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-30 21:06:47 -0700

    initial folder setup with schema and seeds

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('In-N-Out Double Double', true),
+    ('McDonalds Big Mac', false),
+    ('Wendys Baconator', false),
+    ('Burger King Whopper', false)
