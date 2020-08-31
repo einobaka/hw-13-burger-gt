d185cf503fcfe2efa4eaddbdb364354e2ea94758
einobaka <61556425+einobaka@users.noreply.github.com>
2020-08-30 22:37:14 -0700

    finished server.js setup, sucessfully connects to server

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "!Q2w3e123",
   database: "burgers_db"
 });
 
--- server.js
+++ server.js
@@ -1,1 +1,21 @@
+//Dependencies
 const express = require('express')
+const app = express();
+
+// Sets up the Express app to handle data
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Handlebars
+const exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+// Port handling
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
