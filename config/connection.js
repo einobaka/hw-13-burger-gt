8cd6be43783e8c376f60eaf1a270d561745249ac
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-08 15:17:26 -0700

    modified connection for ClearDB

--- config/connection.js
+++ config/connection.js
@@ -3,12 +3,19 @@
 // MySQL connect
 const mysql = require("mysql");
 
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "!Q2w3e123",
+//   database: "burgers_db"
+// });
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
+  HOST: "us-cdbr-iron-east-02.cleardb.net",
+  USER: "b7e2437887xxxa",
+  PASSWORD: "0200xxx6",
+  DB: "heroku_7643ec736354xxx"
 });
 
 // DB connection
