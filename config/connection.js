bc17b0a11fda95c8be24b5257842e672f1149ab2
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-11 17:26:02 -0700

    modified connection

--- config/connection.js
+++ config/connection.js
@@ -7,11 +7,11 @@ const mysql = require("mysql");
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  const connection = mysql.createConnection({
+  var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: "!Q2w3e123",
     database: "burgers_db"
   });
 }
