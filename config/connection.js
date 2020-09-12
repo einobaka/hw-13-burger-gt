33903f9ee95db36148378a56df85bb09e3963aa1
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-11 17:23:17 -0700

    modified connection, using jaws DB

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,18 @@
 // MySQL connect
 const mysql = require("mysql");
 
-// const connection = mysql.createConnection({
-//   host: "localhost",
-//   port: 3306,
-//   user: "root",
-//   password: "!Q2w3e123",
-//   database: "burgers_db"
-// });
 
-const connection = mysql.createConnection({
-  host: "us-cdbr-east-02.cleardb.com",
-  user: "b8797704ed84f6",
-  password: "6aabdc0e",
-  database: "heroku_b47f60853593882"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 // DB connection
 connection.connect(function (err) {
